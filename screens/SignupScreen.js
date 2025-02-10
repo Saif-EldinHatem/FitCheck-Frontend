b1a559a3a67a7ac3685ad1f054bc27eaaae9ea8a
Saif Hatem <saifhatem76@gmail.com>
2025-02-10 05:01:55 +0200

    Fix case sensitivity issue for SignupScreen.js

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -34,7 +34,6 @@ const styles = StyleSheet.create({
     justifyContent: "flex-end",
     alignItems: "center",
     paddingBottom: 12,
-    // backgroundColor: "red",
   },
   star: {
     width: 70,
