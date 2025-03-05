389ecd5f12c83b249f311207a9d58df1ec26e940
Muhab <swe.muhabfawzy@gmail.com>
2025-03-05 02:40:01 +0200

    - Added .env variables

--- App.js
+++ App.js
@@ -28,36 +28,36 @@ export default function App() {
   }
 
   return (
-    // <NavigationContainer>
-    //   <StatusBar style="auto" />
-    //   <Stack.Navigator
-    //     screenOptions={{
-    //       headerShown: false,
-    //       contentStyle: { backgroundColor: colors.main },
-    //       headerStyle: { backgroundColor: colors.main},
-    //       headerTitleAlign: "center",
-    //     }}
-    //   >
-    //     <Stack.Screen name="Signup" component={SignupScreen} />
-    //     <Stack.Screen name="Login" component={LoginScreen} />
-    //     <Stack.Screen
-    //       name="UserRegisteration"
-    //       component={UserRegisterationScreen}
-    //     />
-    //     <Stack.Screen
-    //       name="Verification"
-    //       component={VerificationScreen}
-    //       options={{
-    //         headerShown: true,
-    //         title: "Verify Account",
-    //       }}
-    //     />
-    //   </Stack.Navigator>
-    // </NavigationContainer>
-    <>
-    <StatusBar style="auto"/>
-    <NavBar/>
-    </>
+    <NavigationContainer>
+      <StatusBar style="auto" />
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+          contentStyle: { backgroundColor: colors.main },
+          headerStyle: { backgroundColor: colors.main},
+          headerTitleAlign: "center",
+        }}
+      >
+        <Stack.Screen name="Signup" component={SignupScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen
+          name="UserRegisteration"
+          component={UserRegisterationScreen}
+        />
+        <Stack.Screen
+          name="Verification"
+          component={VerificationScreen}
+          options={{
+            headerShown: true,
+            title: "Verify Account",
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+    // <>
+    // <StatusBar style="auto"/>
+    // <NavBar/>
+    // </>
     // <ItemDetailScreen/>
   );
 }
