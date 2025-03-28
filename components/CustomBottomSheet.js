289af805ca1bb7580a682a66e6899b2eebfc7c3b
Saif Hatem <saifhatem76@gmail.com>
2025-03-29 01:45:02 +0200

    Added bottom sheet to wardrobe

---
+++ components/CustomBottomSheet.js
@@ -0,0 +1,42 @@
+import { StyleSheet, View, Text } from "react-native";
+import {
+  BottomSheetModal,
+  BottomSheetView,
+  BottomSheetBackdrop,
+  BottomSheetScrollView,
+} from "@gorhom/bottom-sheet";
+import { forwardRef } from "react";
+
+const CustomBottomSheet = forwardRef(({ onSheetChanges, children }, ref) => {
+  return (
+    <BottomSheetModal
+      ref={ref}
+      onChange={onSheetChanges}
+      snapPoints={["25%", "50%", "75%", "95%"]}
+      index={2}
+      backdropComponent={(props) => (
+        <BottomSheetBackdrop
+          {...props}
+          disappearsOnIndex={-1}
+          appearsOnIndex={0}
+          opacity={0.5}
+        />
+      )}
+      backgroundStyle={{ backgroundColor: "#EBDFCF" }}
+      // handleIndicatorStyle={{ backgroundColor: colors.accent }}
+    >
+      <BottomSheetScrollView contentContainerStyle={styles.bottomSheetView}>{children}</BottomSheetScrollView>
+    </BottomSheetModal>
+  );
+});
+
+const styles = StyleSheet.create({
+  bottomSheetView: {
+    paddingTop: 20,
+    paddingHorizontal: 20,
+    gap: 15,
+    // backgroundColor: "green",
+  }
+});
+
+export default CustomBottomSheet;
