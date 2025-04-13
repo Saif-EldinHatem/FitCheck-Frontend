3c400775e1567fe6e676ceb79683aa7c4c656ac3
Saif Hatem <saifhatem76@gmail.com>
2025-04-13 13:19:54 +0200

    edied Pill component

--- screens/GenerationScreen.js
+++ screens/GenerationScreen.js
@@ -1,14 +1,42 @@
 import { StyleSheet, View, Text, SafeAreaView, Button } from "react-native";
 import colors from "../assets/colors/colors";
+import FilterPill from "../components/FilterPill";
+import { useState } from "react";
+import Pill from "../components/you screen/Pill";
 import { useNavigation } from "@react-navigation/native";
-import GeneratedOutfitScreen from "./GeneratedOutfitScreen";
 
 function GenerationScreen() {
+  const [dressCode, setDressCode] = useState();
+  const [style, setStyle] = useState();
+  const [theme, setTheme] = useState();
+
   const navigation = useNavigation();
   return (
     <SafeAreaView style={styles.screen}>
-      <Text>GenerationScreen</Text>
-      <Button title="ay haga" onPress={navigation.push("GeneratedOutfit")}/>
+      <View style={styles.generationSection}>
+        <Text style={styles.generationHeader}>What's the dress code</Text>
+        <View style={styles.generationInput}>
+          <Pill
+            title={"Casual"}
+            isSelected={"Casual" === dressCode}
+            setIsSelected={setDressCode}
+          />
+          <Pill
+            title={"Formal"}
+            isSelected={"Formal" === dressCode}
+            setIsSelected={setDressCode}
+          />
+          <Pill
+            title={"Semi-Formal"}
+            isSelected={"Semi-Formal" === dressCode}
+            setIsSelected={setDressCode}
+          />
+        </View>
+      </View>
+      <Button
+        title="Generate"
+        onPress={() => navigation.push("GeneratedOutfit")}
+      />
     </SafeAreaView>
   );
 }
@@ -19,9 +47,19 @@ const styles = StyleSheet.create({
     backgroundColor: colors.main,
     paddingHorizontal: 16,
     paddingTop: 10,
-
-    // justifyContent: "center",
-    // alignItems: "center",
+  },
+  generationSection: {
+    gap: 15,
+  },
+  generationHeader: {
+    fontFamily: "inter-medium",
+    fontSize: 18,
+    lineHeight: 20,
+  },
+  generationInput: {
+    flexDirection: "row",
+    gap: 4,
+    paddingBottom: 5,
   },
 });
 
