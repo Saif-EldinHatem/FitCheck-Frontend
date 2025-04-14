01f8c54687280dd2997fe737e8f1f099502b74b6
samehmohsenn <samehwalid@icloud.com>
2025-04-14 11:12:09 +0200

    updated camera floating bubble in wardrobe screen

--- screens/WardrobeScreen.js
+++ screens/WardrobeScreen.js
@@ -455,7 +455,7 @@ const styles = StyleSheet.create({
     padding: 10,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "white",
+    backgroundColor: colors.mainDark,
   },
   bottomSheetHeader: {
     flexDirection: "row",
